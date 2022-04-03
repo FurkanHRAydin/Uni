0b8a30f29c456f3ac3a60f3c80ced88d52bda35f
furkan <furkan.h.aydin@hotmail.de>
2022-04-03 14:30:53 +0200

    upload

--- Webdev/Ue3/script.js
+++ Webdev/Ue3/script.js
@@ -24,39 +24,44 @@ function order() {
 
     for (x of arr_rezept) {
         if(x.checked) {
-            price += pizza_price(pizza);
-            calories+= pizza_calorie(pizza);
+            price += pizza_price[pizza];
+            calories+= pizza_calorie[pizza];
             zutaten += x.value + " ";
         }
+    }
     if (!arr_rezept[0].checked && !arr_rezept[1].checked && !arr_rezept[2].checked && !arr_rezept[3].checked) {
             alert("Zutat auswaehlen");
-            return;
-        }
+            //return;
+
+    }
+    else {
+        var tr = document.createElement('tr');   //erstellt neue Zeile
+        var td_size = document.createElement('td'); //erstellt neue Spalten nach je nach reihenfogle
+        var td_receipe = document.createElement('td');
+        var td_price = document.createElement('td');
+        var td_calories = document.createElement('td');
+
+        td_size.innerHTML = pizza_size[pizza];
+        td_receipe.innerHTML = zutaten;
+        td_price.innerHTML = pizza_price[pizza];
+        td_calories.innerHTML = pizza_calorie[pizza];
+
+        //fuegt der Zeile Spalten hinzu
+        tr.appendChild(td_size); 
+        tr.appendChild(td_receipe);
+        tr.appendChild(td_price);
+        tr.appendChild(td_calories);
+
+        //fuegt zeile hinzu
+
+        document.getElementById("Zutaten").appendChild(tr);
+
+        console.log(document.getElementById("price").innerHTML);
+
+        document.getElementById("price").innerHTML = parseInt(document.getElementById("price").innerHTML) + pizza_price[pizza];
     }
 
-    var tr = document.createElement('tr');   //erstellt neue Zeile
-    var td_size = document.createElement('td'); //erstellt neue Spalten nach je nach reihenfogle
-    var td_receipe = document.createElement('td');
-    var td_price = document.createElement('td');
-    var td_calories = document.createElement('td');
-
-    td_size.innerHTML = pizza_size[pizza];
-    td_receipe.innerHTML = zutaten;
-    td_price.innerHTML = pizza_price[pizza];
-    td_calories.innerHTML = pizza_calorie[pizza];
-
-    //fuegt der Zeile Spalten hinzu
-    tr.appendChild(td_size); 
-    tr.appendChild(td_receipe);
-    tr.appendChild(td_price);
-    tr.appendChild(td_calories);
-
-    //fuegt zeile hinzu
-
-    document.getElementById("Zutaten").appendChild(tr);
-
-    console.log(document.getElementById("price").innerHTML);
-    document.getElementById("price").innerHTML = parseInt(document.getElementById("price").innerHTML) + pizza_price[pizza];
+    
 }
 
 
